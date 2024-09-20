cb01cd8502a0cf13c03bd5bdbd96c661b4756a9f
KamalLahloh <kamal.lahloh@gmail.com>
2024-09-20 23:17:13 +0300

    openAPI and swagger Create and Config

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -74,4 +74,3 @@ export class EpisodesController {
     return this.episodesService.create(input);
   }
 }
-
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// openAPI and swagger
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookstore')
+    .setDescription('API documentation')
+    .setVersion('1.0.0')
+    .build();
+
+  // create the swagger doc.
+  const document = SwaggerModule.createDocument(app, config);
+  // config the swagger site
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
